8ccf1639cb8e6b57befb5fc72b06720c9a1209df
Vani <62070669+VaniRajaguru@users.noreply.github.com>
2025-05-30 12:12:53 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class HelloGuys{
   public static void main(String[] args){
-    System.out.println("Hello Vani");
+    
     System.out.println("Hello Vidhu");
     System.out.println("Hello Hani");
     System.out.println("Hello Jivi");
